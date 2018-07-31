55762a468ebaaccf475f8b0dffa73f282d8b2e99
TinManTex <tinman.squad@gmail.com>
2018-07-31 12:10:01 +1200

    filter option

--- Uniquify/Program.cs
+++ Uniquify/Program.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace Uniquify {
     class Program {
@@ -14,19 +13,37 @@ namespace Uniquify {
             }
 
             string path = args[0];
+            bool filterChars = false;//DEBUGNOW
+            if (args.Length > 1) {
+                filterChars = args[1] == "filter";
+            }
 
             if (File.Exists(path) == false && Directory.Exists(path) == false) {
                 Console.WriteLine("Could not find file or path " + path);
                 return;
             }
 
-            Uniquify(path);
+            Uniquify(path, filterChars);
         }
 
-        static public void Uniquify(string path) {
+        static List<char> characters = new List<char>{
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '_', '-',
+            '/',
+            '\\',
+            '`','~','!','@','#','$','%','^','&','*','(',')','+','=','[','{',']','}',';',':','?','>','<',
+            '.',
+        };
+
+
+        static public void Uniquify(string path, bool filterChars) {
+
+
             HashSet<string> stringHashSet = new HashSet<string>();
 
-            List<string> stringList = stringHashSet.ToList<string>();
+            
 
             string[] files = null; 
             if (File.Exists(path)) {
@@ -37,24 +54,60 @@ namespace Uniquify {
             }
 
             foreach (string filePath in files) {
+                Console.WriteLine(filePath);
                 string line;
                 using (StreamReader inputText = new StreamReader(filePath)) {
                     while ((line = inputText.ReadLine()) != null) {
-                        stringHashSet.Add(line);
+                        if (filterChars)
+                        {
+                            bool add = true;
+                            foreach (char character in line)
+                            {
+                                if (!characters.Contains(character))
+                                {
+                                    add = false;
+                                }
+                            }
+
+                            
+
+                            if (add)
+                            {
+                                stringHashSet.Add(line);
+                            }
+                        }
+                        else
+                        {
+                            stringHashSet.Add(line);
+                        }
                     }
                 }
             }
 
-            stringList = stringHashSet.ToList<string>();
-            stringList.Sort();
+            string[] stringList = stringHashSet.ToArray<string>();
+            stringList = stringList.OrderBy(s => s, StringComparer.InvariantCultureIgnoreCase).ToArray<string>();
 
             string outPath = Path.GetDirectoryName(path) + "\\";
             string outFileName = Path.GetFileNameWithoutExtension(path) + "_unique.txt";
+            if (filterChars)
+            {
+                outFileName = Path.GetFileNameWithoutExtension(path) + "_unique_filtered.txt";
+            }
             outPath += outFileName;
             outPath = outPath.Replace("\\", "/");
-            File.WriteAllLines(outPath, stringList.ToArray());
+            File.WriteAllLines(outPath, stringList);
         }
 
+        public class StrCmpLogicalSort : IComparer<string> {
+
+            [DllImport("shlwapi.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
+            static extern int StrCmpLogicalW(String x, String y);
+
+            public int Compare(string x, string y) {
+                return StrCmpLogicalW(x, y);
+            }
+
+        }
 
     }
 }
